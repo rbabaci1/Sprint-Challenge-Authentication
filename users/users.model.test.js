36d683b71c8af2b7a3d746cf3b27f988f2bdf9ad
rabah babaci <4rabah@gmail.com>
2020-06-13 11:11:50 -0700

    add a test to check if the insert model returns the inserted user

--- users/users.model.test.js
+++ users/users.model.test.js
@@ -1,20 +1,28 @@
-const request = require("supertest");
-
 const db = require("../database/dbConfig");
 const Users = require("./users-model");
+const mockData = require("../utils/mockData");
 
 describe("testing the users models", () => {
   it("", () => {});
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
 
-  //   beforeEach(async () => {
-  //     await db("users").truncate();
-  //   });
-  //   describe("insert()", () => {
-  //     it("should insert a user to the DB", async () => {
-  //       await Users.insert({ username: "rbabaci1", password: "rabah" });
-  //       await Users.insert({ username: "kyla97", password: "kyla" });
-  //       const users = await db("users");
-  //       expect(users).toHaveLength(2);
-  //     });
-  //   });
+  describe("insert()", () => {
+    it("should insert a user to the DB", async () => {
+      await Users.insert(mockData[0]);
+      await Users.insert(mockData[1]);
+
+      const users = await db("users");
+      expect(users).toHaveLength(2);
+    });
+
+    it("should return the added user", async () => {
+      let insertedUser = await Users.insert(mockData[0]);
+      expect(insertedUser.username).toBe("rbabaci1");
+
+      insertedUser = await Users.insert(mockData[1]);
+      expect(insertedUser.username).toBe("kyla97");
+    });
+  });
 });
---
+++ utils/mockData.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { username: "rbabaci1", password: "rabah" },
+  { username: "kyla97", password: "kyla" },
+];
