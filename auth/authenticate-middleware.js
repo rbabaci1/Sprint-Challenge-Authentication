5710aa62a97ae7463b2570f6f8e2b53cd83020c6
rabah babaci <4rabah@gmail.com>
2020-06-13 10:07:15 -0700

    implement the autheticat middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,22 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+
+const { jwtSecret } = require("../utils/secrets");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  try {
+    const token = req.headers.authorization;
+
+    if (token) {
+      jwt.verify(token, jwtSecret, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ message: "Invalid credential. Try again?" });
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "shall not pass!", reason: error.message });
+  }
 };
