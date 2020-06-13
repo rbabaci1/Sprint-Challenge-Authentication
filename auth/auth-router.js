b353ec64bda54736d7e22f8c2a2a7f7144fc3164
rabah babaci <4rabah@gmail.com>
2020-06-13 09:34:41 -0700

    implement the post register user endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,24 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
-router.post("/register", (req, res) => {
-  // implement registration
+const Users = require("../users/users-model");
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+
+    const addedUser = await Users.insert({ ...user, password: hash });
+
+    res.status(201).json(addedUser);
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "This user could not be added at this moment.",
+        reason: error.message,
+      });
+  }
 });
 
 router.post("/login", (req, res) => {
