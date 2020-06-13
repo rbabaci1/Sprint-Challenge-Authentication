2f5aac0a39bb42427fc6e1532ea484001be030da
rabah babaci <4rabah@gmail.com>
2020-06-13 11:41:21 -0700

    reset database

--- database/seeds/01-users.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function (knex) {
-  return knex("users")
-    .del()
-    .then(function () {
-      return knex("users").insert([
-        { id: 1, username: "rabah", password: "babaci" },
-      ]);
-    });
-};
